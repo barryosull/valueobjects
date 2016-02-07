f2b7ca08e179c79817206d372e13faecd554b478
Barry O Sullivan <barry@tercet.io>
2016-02-07 16:01:38 +0000

    Adding docblock comments

--- src/Contract/Validator.php
+++ src/Contract/Validator.php
@@ -4,7 +4,16 @@ namespace EventSourced\Contract;
 
 interface Validator 
 {
+    /**
+     * Check if the arguments pass validation
+     * 
+     * @param type $arguments
+     */
     public function is_valid($arguments);
-    
+        
+    /** 
+     * This error message is not intended for display to users, 
+     * it is merely used to give more context to Exceptions
+     */
     public function error_message();
 }
