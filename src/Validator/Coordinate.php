6df56ee66e78ddaaa7e233b830c6ab1b2d8cff5e
Barry O Sullivan <barry@tercet.io>
2016-02-08 18:24:51 +0000

    Tweaking whitespace

--- src/Validator/Coordinate.php
+++ src/Validator/Coordinate.php
@@ -11,8 +11,8 @@ class Coordinate extends Wrapper implements Contract\Validator
     public function __construct()
     {
         $validator = (new Validator\Float())
-                ->and_x(new Validator\GreaterThanOrEqual(-90))
-                ->and_x(new Validator\LessThanOrEqual(90));
+            ->and_x(new Validator\GreaterThanOrEqual(-90))
+            ->and_x(new Validator\LessThanOrEqual(90));
         
         parent::__construct($validator);
     }
