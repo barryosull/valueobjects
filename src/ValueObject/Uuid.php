9074b33ea5edc70f9174ef03f728b849cbe92fb7
vagrant <vagrant@dynamicres>
2016-09-27 14:07:09 +0000

    Add new dependency. Added helper factory method to UUID.

--- src/ValueObject/Uuid.php
+++ src/ValueObject/Uuid.php
@@ -1,11 +1,10 @@
-<?php
-
-namespace EventSourced\ValueObject\ValueObject;
+<?php namespace EventSourced\ValueObject\ValueObject;
 
 use EventSourced\ValueObject\Contracts\ValueObject\Identifier;
+use Rhumsaa\Uuid\Uuid as UuidGenerator;
 
 class Uuid extends Type\AbstractSingleValue implements Identifier
-{    
+{
     protected function validator()
     {
         return parent::validator()->regex('/([a-f\d]{8}(-[a-f\d]{4}){3}-[a-f\d]{12}?)/i');
@@ -14,5 +13,10 @@ class Uuid extends Type\AbstractSingleValue implements Identifier
     public function is_null()
     {
         return $this->equals(new Uuid('00000000-0000-0000-0000-000000000000'));
+    }
+
+    public static function generate()
+    {
+        return new Uuid( UuidGenerator::uuid4()->toString() );
     }
 }
