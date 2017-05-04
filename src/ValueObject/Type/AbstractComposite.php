0a904f2707bf4caa80b1eab75a0c8d252d3f6909
Barry O Sullivan <barry@tercet.io>
2017-05-04 11:45:38 +0100

    Refactored messy code.

--- src/ValueObject/Type/AbstractComposite.php
+++ src/ValueObject/Type/AbstractComposite.php
@@ -8,27 +8,34 @@ abstract class AbstractComposite extends AbstractValueObject
 {  
     public function equals(ValueObject $other_valueobject) 
 	{
-        $result = $this->is_same_class($other_valueobject);
-        if (!$result) {
-            return $result;
+        if (!$this->is_same_class($other_valueobject)) {
+            return false;
         }
-        foreach ($this as $key=>$valueobject) {
 
-            if ($valueobject === null) {
-                $result = $result && ($other_valueobject->$key === null);
-            } else {
-                $result = $result && $valueobject->equals($other_valueobject->$key);
+        foreach ($this as $key=>$valueobject) {
+            if (!$this->vos_are_eqauls($valueobject, $other_valueobject->$key)) {
+                return false;
             }
         }
-		return $result;
+		return true;
 	}
+
+	private function vos_are_eqauls($valueobect_a, $valueobect_b)
+    {
+        if ($valueobect_a === null) {
+            return ($valueobect_b === null);
+        }
+
+        return $valueobect_a->equals($valueobect_b);
+    }
 
     public function is_null()
     {
-        $result = true;
         foreach ($this as $key=>$valueobject) {
-            $result = $result && ($valueobject === null);
+            if ($valueobject !== null) {
+                return false;
+            }
         }
-        return $result;
+        return true;
     }
 }
